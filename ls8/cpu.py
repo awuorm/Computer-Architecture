301c8999140589e6048775eed03402853c113d5f
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-03-23 18:57:39 +0300

    started on CPU class

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc=0  # Program Counter address of current executing instruction
+        self.reg=[0]*7 # General Purpose Registers
+        self.reg.append(0xF4) # 256 bit Storage
+        self.ram=[0]*255 # This will serve as 256 Bytes of RAM storage
+        self.IR=0 # Instruction Register running Instruction
+        self.sp = 7 # Stack pointer Pointing at the top of the stack
+        """Both Are needed to keep track of where in memory we are, 
+            as well as what is being stored"""
+        self.MAR=0 # Memory Address Register 
+        self.MDR=0 # Memory Data Register
+        self.FL=0 # Flags given based on CMP opcode
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +72,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        while running is True:
+            self.IR = self.ram_read(self.pc)
+            if self.IR==0b00000001:
+                #HLT
+                running=False
+            elif self.IR == 0b10000010:
+                     #LDI
+                self.pc += 3
+            elif self.IR == 0b01000111:
+                 #PRN
+                print(8)
+                self.pc += 1
+            else:
+                print("Invalid Instruction")
+                running = False
+
+
+
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR]=MDR
